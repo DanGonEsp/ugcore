c930a7c1189a7f00b3bfe2377e54ea36ab8fd310
Dmitry Logashenko <LogashenkoDL@gmail.com>
2026-06-22 21:59:48 +0300

    Setting element for user data in the error estimators.

--- ugbase/lib_disc/spatial_disc/user_data/data_evaluator_impl.h
+++ ugbase/lib_disc/spatial_disc/user_data/data_evaluator_impl.h
@@ -389,6 +389,12 @@ void DataEvaluatorBase<TDomain, TElemDisc>::prepare_err_est_elem
 (LocalVector& u, GridObject* elem, const MathVector<dim> vCornerCoords[],
 const LocalIndices& ind, bool bDeriv)
 {
+// 	NOTE: constant data is not processed, since constant == independent of the element
+	for(size_t i = 0; i < m_vPosData.size(); ++i)
+		m_vPosData[i]->set_element(elem);
+	for(size_t i = 0; i < m_vDependentData.size(); ++i)
+		m_vDependentData[i]->set_element(elem);
+	
 	try
 	{
 		for (size_t i = 0; i < m_vElemDisc[PT_ALL].size(); ++i)
